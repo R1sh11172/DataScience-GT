0c9699248ab8ce70562667f559981aaf18d3d40c
R1sh11172 <85463644+R1sh11172@users.noreply.github.com>
2023-02-06 10:44:15 -0500

    Update Week1.py

--- Week1.py
+++ Week1.py
@@ -67,7 +67,7 @@ def decrypt(message, shift):
     chars = []
     offset = shift
     offset = offset % 26
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
     for i in range(x):
         if a[i] != " ":
